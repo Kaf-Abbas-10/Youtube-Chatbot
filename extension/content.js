fd536a490d3461dc4fd0345a7c920b9aebf50ea2
kaf-abbas-10 <kafabbas10@gmail.com>
2025-12-11 20:33:54 +0500

    Error Removal

---
+++ extension/content.js
@@ -0,0 +1,7 @@
+// This script runs on YouTube pages
+// It can be used to inject UI elements or extract additional information
+
+console.log('YouTube Video Chat extension loaded');
+
+// You can add functionality here to inject a chat button on the YouTube page itself
+// For now, the extension works through the popup
